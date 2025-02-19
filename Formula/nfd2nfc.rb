fe6c0a4ed8bd0a28462636c5252466bc5858506a
elgar328 <cork_unsold_0k@icloud.com>
2025-02-19 21:29:05 +0900

    test

--- Formula/nfd2nfc.rb
+++ Formula/nfd2nfc.rb
@@ -2,7 +2,7 @@ class Nfd2nfc < Formula
   desc "Convert filesystem entry names from NFD to NFC for cross-platform compatibility"
   homepage "https://github.com/elgar328/nfd2nfc"
   url "https://github.com/elgar328/nfd2nfc/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "18ebb9ce69f1b0cd2c1cd3637da1a685c5589e03c6d1adfea52978746a26f1a4"
+  sha256 "ea46c2b30cef71a3366e4fea451d7f4ddf21f66b24ce7b37ef8a38bac2ecaa08"
   license "MIT"
   head "https://github.com/elgar328/nfd2nfc.git", branch: "main"
 
@@ -22,9 +22,6 @@ class Nfd2nfc < Formula
       working_dir HOMEBREW_PREFIX
     end
 
-    # def post_install
-    #   system "launchctl", "load", "-w", plist_path.to_s
-    # end
     def post_install
       system "brew", "services", "start", "nfd2nfc"
       system "launchctl", "load", "-w", plist_path
