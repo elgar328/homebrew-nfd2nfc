66c85d0a0f74895a09a7b5d6f8971a13fa2d94e8
elgar328 <elgar328@users.noreply.github.com>
2026-02-06 02:05:58 +0900

    Fix download URL: remove v prefix from filename

--- Formula/nfd2nfc.rb
+++ Formula/nfd2nfc.rb
@@ -1,7 +1,7 @@
 class Nfd2nfc < Formula
   desc "Convert filesystem entry names from NFD to NFC for cross-platform compatibility"
   homepage "https://github.com/elgar328/nfd2nfc"
-  url "https://github.com/elgar328/nfd2nfc/releases/download/v2.0.0/nfd2nfc-v2.0.0-universal-apple-darwin.tar.gz"
+  url "https://github.com/elgar328/nfd2nfc/releases/download/v2.0.0/nfd2nfc-2.0.0-universal-apple-darwin.tar.gz"
   sha256 "579c9a0e2d944d7f85a2010100f7975348b3e3f0f36d511b4b642db6df38b4e2"
   license "MIT"
 
